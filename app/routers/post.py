9291fe094652b3c3a580f7f34f776fce898760b5
jilu <jilutech@gmail.com>
2022-10-24 22:13:13 +0530

    added Procfile

--- app/routers/post.py
+++ app/routers/post.py
@@ -44,8 +44,6 @@ def create_posts(post: schemas.PostCreate, db: Session = Depends(get_db), curren
     # new_post = cursor.fetchone()
 
     # conn.commit()
-    print('hi')
-    print(current_user.id)
 
     new_post = models.Post(owner_id=current_user.id, **post.dict())
     db.add(new_post)
